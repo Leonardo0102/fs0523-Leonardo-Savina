ab1d33b2c1775130cddda52a91ac40abb0717284
Leonardo0101 <savinaleonardo@gmail.com>
2023-11-08 19:57:48 +0000

    esercizio-completo

--- m2/s3/g3/app.js
+++ m2/s3/g3/app.js
@@ -1,20 +1,45 @@
-fetch('https://striveschool-api.herokuapp.com/books')
-.then((res)=> res.json())
-.then((res)=>{
-    res.forEach(immagine => {
-        let img=document.createElement('img');
-        img.src=immagine.img;
-        document.querySelector('.card').appendChild(img);
-        
+let cartStorage=localStorage.getItem('cart');
+let cartArray=cartStorage?JSON.parse(cartStorage):[];
+console.log(cartArray);
+
+fetch("https://striveschool-api.herokuapp.com/books")
+  .then((res) => res.json())
+  .then((libri) => {
+    let target = document.querySelector("#target");
+    libri.forEach((libro) => {
        
-        
+       
+        //creo un clone
+      let clone = getCardClone();
+       
+      
+      
+      //identifico i pezzi con cui interagire
+      let img = clone.querySelector(".card-img-top");
+      let title = clone.querySelector(".card-title");
+
+      let price = clone.querySelector(".card-price");
+      let addTocart= clone.querySelector('.addToCart');
+       
+      
+      
+      //modifico i pezzi appe identificati
+      img.src = libro.img;
+      title.innerText = libro.title;
+
+      price.innerText = libro.price;
+      addTocart.addEventListener('click',()=>{
+        cartArray.push(libro);
+        localStorage.setItem('cart',JSON.stringify(cartArray));
+      })
+
+
+      //faccio si che questi cloni appaiano
+      target.append(clone);
     });
+  });
 
-})
-
-const newListItem=document.createElement('div');
-newListItem.classList.add('card');
-
-document.querySelector('.container').appendChild(newListItem);
-
-const div=document.querySelector('.aggiungi')
+function getCardClone() {
+  let temp = document.getElementsByTagName("template")[0];
+  return temp.content.cloneNode(true);
+}
