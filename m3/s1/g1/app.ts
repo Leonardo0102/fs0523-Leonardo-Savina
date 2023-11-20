3e1f3da4e157ce54acfdbf0f5a020f5bfb5a230e
Leonardo0101 <savinaleonardo@gmail.com>
2023-11-20 15:32:12 +0000

    esercizio typescript

---
+++ m3/s1/g1/app.ts
@@ -0,0 +1,26 @@
+const numeroCaso:number=numeroCasuale(1,100)
+function numeroCasuale(min:number,max:number):number{
+    return Math.floor(Math.random()*(max-min)+min);
+}
+console.log(numeroCaso)
+
+function vincitore(numeroCaso:number,giocatore1:number,giocatore2:number):string|void{
+    const persona1= Math.abs(numeroCaso - giocatore1);
+    const persona2=Math.abs(numeroCaso - giocatore2);
+    if(giocatore1===numeroCaso){
+        return 'il giocaotore 1 ha indovinato'
+    } else if(giocatore2===numeroCaso){
+        return 'il giocaotore 2 ha indovinato'
+    }
+
+    if(persona1<persona2){
+        return ' giocatore1 si è avvicinato di più al numero'
+    } else if(persona2<persona1){
+        return 'giocaotre2 si è avvicinato di più al numero'
+    }
+}
+const giocatore1:number=10
+const giocatore2:number=20
+const risultatoFinale:string|void=vincitore(numeroCaso, giocatore1,giocatore2)
+
+console.log(risultatoFinale) 
